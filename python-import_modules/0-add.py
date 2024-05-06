f7c19476e7636c374366051632d4d957a314e950
Jenna Jones <jljones121@gmail.com>
2024-05-06 19:17:49 +0000

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     a = 1
     b = 2
-    add_0 = __import__('add_0')
-    result = add_0.add(a, b)
+    from add_0 import add
+    result = add(a, b)
     print('{} + {} = {}'.format(a, b, result))
