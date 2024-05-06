ae7c9345f035b295608802ceb8f2233dc9d0240b
Jenna Jones <jljones121@gmail.com>
2024-05-06 14:12:31 +0000

    print hexa

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 print("".join(["{}".format(chr(x))
 
-    for x in range(ord('a'), ord('z')+1)]), end='')
+               for x in range(ord('a'), ord('z') + 1)]), end='')
---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+print("\n".join(["Decimal: {}, Hexadecimal: {}".format(
+    num, hex(num)) for num in range(99)]))
