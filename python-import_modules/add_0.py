e497bb476d0e16f970c0cafd5facd62888a703bd
Jenna Jones <jljones121@gmail.com>
2024-05-06 19:11:50 +0000

    0.

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-add_0 = __import__("add_0")
-result = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
