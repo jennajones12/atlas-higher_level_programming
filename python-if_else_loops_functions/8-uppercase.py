4e82cb1fdf0501395cda6e49c57ac326fe8b7bcc
Jenna Jones <jljones121@gmail.com>
2024-05-06 17:39:46 +0000

    8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def uppercase(s):
-    result = ''
-    for char in s:
-        if 'a' <= char <= 'z':
-            result += chr(ord(char) - 32)
+    result = ""
+    for char in str:
+        ascii_val = ord(char)
+        if 97 <= ascii_val <= 122:
+            result += chr(ascii_val - 32)
         else:
             result += char
-        print(result)
+            print(result)
+            print()
