1be2a6e1ec85870913f64655577a10232bbe388f
Jenna Jones <jljones121@gmail.com>
2024-05-06 07:36:16 +0000

    print

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-number = "333"
-print(f"{number[0:3]} Battery street")
+number = "98 333"
+print(f"{number[:2]} Battery street")
+print(f"{number[3:]} Battery street")
+print(f"-{number[:2]} Battery street")
