ffccaf439dcdfbb688791513234824d4efabb12c
Jenna Jones <jljones121@gmail.com>
2024-05-06 23:11:48 +0000

    3infinite

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    from sys import argv
-
-    arguments = argv[1:]
-
-    total = 0
-
-    for arg in arguments:
-        total += int(arg)
-
-        print(total)
+    result = sum(int(arg) for arg
+in sys.argv[1:])
+    print(result)
