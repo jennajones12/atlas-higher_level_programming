df546308330c8310b42920e0abf950853f898c75
Jenna Jones <jljones121@gmail.com>
2024-05-06 14:36:10 +0000

    new code to print desired outpout

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}, ".format(i * 10 + j, end=""))
+        if i != j:
+            if j != 9 or i != 8:
+                print("{:d}{:d}, ".format(i, j), end="")
+            else:
+                print("{:d}{:d}".format(i, j))
