3ace62363b4becceffc524b62bbc7ec728090203
Jenna Jones <jljones121@gmail.com>
2024-05-06 13:55:32 +0000

    pycodestyle

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(["{}".format(chr(x)) for x in range(ord('a'), ord('z')+1)]), end='')
+print("".join(["{}".format(chr(x)) for x in range(ord('a'),
+    ord('z')+1)]), end='')
