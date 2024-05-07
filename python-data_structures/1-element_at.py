0171f33ba76d57b7f549c3908337c06381087313
Jenna Jones <jljones121@gmail.com>
2024-05-07 22:18:53 +0000

    syntax errors

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,6 @@
 def element_at(my_list, idx):
 
     if idx < 0 or idx >= len(my_list):
-        return none
-
-
-else:
-    return my_list[idx]
+        return None
+    else:
+        return my_list[idx]
