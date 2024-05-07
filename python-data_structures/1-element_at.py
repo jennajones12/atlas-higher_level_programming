5f4b56b8c0af3f0a9f5f2ab9fd8729a131e8fa2c
Jenna Jones <jljones121@gmail.com>
2024-05-07 22:15:49 +0000

    typo line 6

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
 
+    if idx < 0 or idx >= len(my_list):
+        return none
 
-if idx < 0 or idx >= len(my_list):
-    reurn none
+
 else:
     return my_list[idx]
