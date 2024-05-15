c1f96c8b1462e4d2c23fa4044f0128f1bb69de35
Jenna Jones <jljones121@gmail.com>
2024-05-15 04:19:57 +0000

    2.3

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
+"""
+Module square
+Defines a class Square.
+"""
 
 
 class Square:
-    """Defines a square that has attributes. Instantiation with size
+    """
+    Defines a square that has attributes. Instantiation with size.
 
     Attributes:
-        size: size of square
+       __size (int): Size of square.
     """
+    
     def __init__(self, size=0):
-        """Initializes the Square instance
+        """
+        Initializes the Square instance.
 
         Args:
-            size: (:obj: 'int', optional): A private instance size
+            size (int): The size of the square (must be an integer and >= 0).
 
         Raises:
-            TypeError: If size is not an integer
-            ValueError: If size is less than 0
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
-
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
