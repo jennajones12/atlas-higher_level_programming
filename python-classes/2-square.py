05c8b7216bedaa495577d6704cd438697c39e3ad
Jenna Jones <jljones121@gmail.com>
2024-05-15 04:06:43 +0000

    1/2]

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 class Square:
     """
     Defines a square
@@ -21,7 +23,7 @@ class Square:
 
 
         Args:
-            size (int): size of the square (default is 0)
+            size: (:obj: 'int', optional): A private instance size
 
 
         Raises:
@@ -35,41 +37,3 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-            print(e)
-
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
