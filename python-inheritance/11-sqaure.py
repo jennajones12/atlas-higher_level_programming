e979b070233ad9968611421b7321368126182406
Jenna Jones <jljones121@gmail.com>
2024-05-29 17:29:36 +0000

    11

---
+++ python-inheritance/11-sqaure.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class"""
+Rectangle = __import__('10-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class square"""
+
+    def __init__(self, size):
+        """Initializes Square"""
+        super().__init__(size, size)
+
+    def __str__(self):
+        """String of Square"""
+        return "[Square] {}/{}".format(self.__side, self.__height)
