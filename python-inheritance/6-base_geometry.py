e0332ae45d206eb8351318ccbd4731066640fb23
Jenna Jones <jljones121@gmail.com>
2024-05-29 11:57:43 +0000

    6base

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
