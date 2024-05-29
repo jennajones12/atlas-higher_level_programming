6f05c0b048bd267b6b83fd2f6b25c0f31f12280d
Jenna Jones <jljones121@gmail.com>
2024-05-29 18:13:27 +0000

    11/3

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -16,7 +16,7 @@ class Square(Rectangle):
         self.__size = size
 
         def area(self):
-            return self.__size * self.__size
+            return f"[Square] {self.__size} * {self.__size}"
 
         def __str__(self):
             return f"[Square] {self.__size}/{self.__size}"
