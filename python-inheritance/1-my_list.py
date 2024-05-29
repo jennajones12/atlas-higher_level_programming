cbfa61caacc03d1b2601df8089e8d65e52f870ea
Jenna Jones <jljones121@gmail.com>
2024-05-29 10:47:52 +0000

    doctesting/code update with repr code

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """Module for MyList class"""
 
-
 class MyList(list):
     """A class that inherits from list and provides additional functionality."""
-    def __init__(self):
-         """initializes the object"""
-         super().__init__()
+    def __init__(self, initial_list=None):
+        """Initializes the object with an optional initial list."""
+        if initial_list is None:
+            super().__init__()
+        else:
+            super().__init__(initial_list)
 
     def print_sorted(self):
         """Prints the list in sorted order (ascending)."""
         print(sorted(self))
+
+    def __repr__(self):
+        """Returns a string representation of the object."""
+        return f"MyList({super().__repr__()})"
