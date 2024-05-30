cfa1b6570a42961c1e4557ae44088c579a6038c4
Jenna Jones <jljones121@gmail.com>
2024-05-30 06:35:43 +0000

    commit

---
+++ python-almost_a_circle/tests/models/__init__.py
@@ -0,0 +1,2 @@
+# models/__init__.py
+# This file is necessary to make 'models' a package.
---
+++ python-almost_a_circle/tests/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Base module
+"""
+
+class Base:
+    """
+    The Base class for all other classes in this project.
+    Manages `id` attribute to avoid duplicating the same code.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialization of a Base instance.
+        
+        Args:
+            id (int): The id of the instance. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
