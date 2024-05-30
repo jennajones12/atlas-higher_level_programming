55c0b4a33c1f5dad15ed4f689a4f2e61586877a0
Jenna Jones <jljones121@gmail.com>
2024-05-30 07:06:26 +0000

    rectable

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Rectangle module
+"""
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    Rectangle class inherits from Base.
+    Represents a rectangle with width, height, x, and y attributes.
+    """
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x coordinate of the rectangle.
+            y (int): The y coordinate of the rectangle.
+            id (int): The id of the instance. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
