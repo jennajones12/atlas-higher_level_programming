2af32979c47ad87e98406191b5bc6b794b3d6ca0
Jenna Jones <jljones121@gmail.com>
2024-05-30 02:19:13 +0000

    12.1

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -17,8 +17,9 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        last_row = triangle[-1]
+        row += [last_row[j] + last_row[j+1] for j in
+                range(len(last_row) - 1)]
         row.append(1)
         triangle.append(row)
 
