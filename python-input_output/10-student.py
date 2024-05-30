273da225e53eca6d97e906e5fd22f7bd1c5367b8
Jenna Jones <jljones121@gmail.com>
2024-05-30 02:06:03 +0000

    10/11 pep8 corrections

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,19 +3,18 @@
 definition of the Student class.
 """
 
+
 class Student:
     """
     class representing a student
     """
-    
 
     def __init__(self, first_name, last_name, age):
         """Initialize Student instance with first name, last name, age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
-    
+
     def to_json(self, attrs=None):
         """
         gets dict representation of Student instance
@@ -31,4 +30,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr) for attr
+                    in attrs if hasattr(self, attr)}
--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -9,14 +9,12 @@ class Student:
     class representing a student
     """
 
-    
     def __init__(self, first_name, last_name, age):
         """Initialize Student instance with first name, last name, age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    
     def to_json(self, attrs=None):
         """
         gets dict representation of Student instance
@@ -33,7 +31,6 @@ class Student:
             return self.__dict__
         else:
             return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-
 
     def reload_from_json(self, json):
         """
