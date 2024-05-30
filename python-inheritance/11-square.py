d030449d3d1e275cbbdfbd5de5b05d3ea0b19191
Jenna Jones <jljones121@gmail.com>
2024-05-30 02:37:25 +0000

    square finally

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
 """defines square, subclass of rectangle"""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
+class Rectangle:
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
 class Square(Rectangle):
-    """represents a square"""
-
     def __init__(self, size):
-        """initializes new square
-
-        size: (int) size of new square
-        """
-        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
-        def area(size):
-            return f"[Square] {self.__size} * {self.__size}"
+    def integer_validator(self):
+        if type(self.__size) != int or self.__size <= 0:
+            raise Exception("size must be a positive integer")
 
-        def __str__(self):
-            return f"[Square] {self.__size}/{self.__size}"
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
