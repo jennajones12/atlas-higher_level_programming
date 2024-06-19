c14cc89687c9bdbfc058c8b311dd9a23955fdc22
Jenna Jones <jljones121@gmail.com>
2024-06-19 05:00:19 +0000

    2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,40 +1,42 @@
 #!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    # Check if all 4 arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
-        sys.exit(1)
-    
-    # Retrieve command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    # Get command-line arguments
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to MySQL server
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
-        cursor = db.cursor()
-    except MySQLdb.Error as e:
-        print("MySQLdb Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-    
-    # Construct SQL query
-    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+    search = sys.argv[4]
+    sql = "SELECT * FROM `states` "
+    sql += "WHERE BINARY `name` = '{}' "
+    sql += "ORDER BY `id`"
 
-    # Execute query with user input
-    try:
-        cursor.execute(query, ('%' + state_name + '%',))
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-    except MySQLdb.Error as e:
-        print("MySQLdb Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-    
+    # Connect to MySQL
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query
+    cursor.execute(sql.format(search))
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Print the results
+    for row in results:
+        print(row)
+
     # Close cursor and database connection
     cursor.close()
     db.close()
