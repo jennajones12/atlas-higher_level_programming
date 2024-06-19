e56c232a776d1f0af9041f4e73cacac3cd8a46ff
Jenna Jones <jljones121@gmail.com>
2024-06-19 05:01:17 +0000

    3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,3 +1,40 @@
 #!/usr/bin/python3
 """
+takes in arguments and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+"""
 
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Get command-line arguments
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    search = sys.argv[4]
+    sql = "SELECT * FROM `states` WHERE `name` = %s ORDER BY `id`"
+
+    # Connect to MySQL
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query
+    cursor.execute(sql, (search,))
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Print the results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
