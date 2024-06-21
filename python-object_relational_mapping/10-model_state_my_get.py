c27256a1add50e6f291633a3fe103bd702b09284
Jenna Jones <jljones121@gmail.com>
2024-06-21 05:00:52 +0000

    10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -8,26 +8,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-def print_states(username, password, db_name, search):
-    """
-    Prints the State object with the name passed as argument from the database hbtn_0e_6_usa.
-    """
 
-    # Create an engine and connect to the MySQL server
+def print_states(username, password, db_name, search):
+    # Create a database connection
     connection = "mysql://{}:{}@localhost:3306/{}"
     engine = create_engine(connection.format(username, password, db_name))
     Base.metadata.create_all(engine)
 
-
-    # Create a configured "Session" class
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the database for the state with the given name
-    state = session.query(State).order_by(State.id).\
+    # Query all State objects and sort by id
+    states = session.query(State).order_by(State.id).\
         filter(State.name == search).all()
 
-    # Print the result
+    # Print the results
     if states:
         for state in states:
             print("{}".format(state.id))
@@ -38,7 +34,7 @@ def print_states(username, password, db_name, search):
     session.close()
 
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
     search = sys.argv[4]
     print_states(username, password, db_name, search)
