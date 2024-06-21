2028297df3bd3bd32cd8d92fd9cda8f2d9f8b635
Jenna Jones <jljones121@gmail.com>
2024-06-21 05:11:57 +0000

    12 documentation

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+A script that changes the name of a State object in the database hbtn_0e_6_usa.
+"""
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -29,4 +33,3 @@ if __name__ == "__main__":
 
     # Close the session
     session.close()
-
