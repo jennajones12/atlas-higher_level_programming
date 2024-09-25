4676aae1eb0d06bf8cfc2f3bad9e19ff6e5329b4
Jenna Jones <jljones121@gmail.com>
2024-09-25 04:59:19 +0000

    update base.py with json_str method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_of_dictionaries):
+        """Converts list of dicts to json str"""
+        if list_of_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_of_dictionaries)
