f3105cc4601f1991b768d092b054e204d9b4326c
Jenna Jones <jljones121@gmail.com>
2024-09-25 05:25:40 +0000

    tests

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Unittests for Base class"""
+
+import unittest
+from models.base import Base
+import os
+
+class TestBase(unittest.TestCase):
+    """Test cases for the Base class"""
