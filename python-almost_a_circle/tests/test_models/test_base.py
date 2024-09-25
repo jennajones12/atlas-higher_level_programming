9c816796e677bd57fda79550c4558bd89ad4d34b
Jenna Jones <jljones121@gmail.com>
2024-09-25 05:33:16 +0000

    test_base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,3 +7,18 @@ import os
 
 class TestBase(unittest.TestCase):
     """Test cases for the Base class"""
+
+    def test_auto_id_assignment(self):
+        """Test if Base() auto assigns ID"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(10)
+        b4 = Base()
+
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 10)
+        self.assertEqual(b4.id, 3)
+
+if __name__ == '__main__':
+    unittest.main()
