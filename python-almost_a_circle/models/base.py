a554b425d6569a421d4c60c4868369f6a6289eeb
Jenna Jones <jljones121@gmail.com>
2024-09-25 03:57:49 +0000

    base.py and init

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""File for class Base"""
+
+class Base:
+    """Base class for shapes"""
+
+    id = 0
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """base init funct"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
