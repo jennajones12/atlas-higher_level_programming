5f095b2dfc49ba4cfd56a7e6a961c3c2a6e3e5cf
Jenna Jones <jljones121@gmail.com>
2024-09-26 05:15:23 +0000

    typo line 8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,4 +4,4 @@
 
 def is_same_class(obj, a_class):
     """Return True if obj is  instance of a_class; otherwise False"""
-    return (some_class is type(obj))
+    return (same_class is type(obj))
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines the is_kind_of_class funct"""
 
+
 def is_kind_of_class(obj, a_class):
     """Returns True if obj is an instance of or inherits from a_class"""
     return (isinstance(obj, a_class))
