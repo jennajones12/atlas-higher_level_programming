c7c7525a2881c0a16552ec047382ba630a5a44db
Jenna Jones <jljones121@gmail.com>
2024-09-26 05:13:35 +0000

    2is same; 3iskindofclass

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,4 +3,4 @@
 
 def is_kind_of_class(obj, a_class):
     """Returns True if obj is an instance of or inherits from a_class"""
-    return (isinstance(obj, some_class))
+    return (isinstance(obj, a_class))
