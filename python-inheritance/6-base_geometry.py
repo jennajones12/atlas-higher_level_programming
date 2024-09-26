bd3dedb16bc66b7938bae43981d765baf804526d
Jenna Jones <jljones121@gmail.com>
2024-09-26 05:21:42 +0000

    4, 5, 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+class BaseGeometry:
+    """documentaiton"""
+
+    def area(self):
+        """documentation"""
+        e = "area() is not implemented"
+        raise Exception(e)
