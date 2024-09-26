50fcaa79dd6fa45e290c71d94df858fdf326ec92
Jenna Jones <jljones121@gmail.com>
2024-09-26 05:08:24 +0000

    2is same

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """Return True if obj is  instance of a_class; otherwise False"""
+
+
+return isinstance(obj,     return )
